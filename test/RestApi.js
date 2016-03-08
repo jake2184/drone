7ff816b03c3b5b27678b947975b0fa172df75d69
Jake <jr2812@ic.ac.uk>
2016-03-08 13:19:59 +0000

    Added basic tests

---
+++ test/RestApi.js
@@ -0,0 +1,53 @@
+require('should');
+require('should-http');
+var assert = require('assert');
+var request = require('supertest');
+
+describe('Routing', function(){
+   var url = 'http://localhost:8080';
+
+    describe("Website", function (){
+    it('should return index.html', function(done){
+         request(url)
+             .get('/')
+             .end(function(err, res){
+                 if(err){
+                     throw err;
+                 }
+                 res.statusCode.should.be.exactly(200);
+                 done();
+             });
+     });
+   });
+
+   describe("Getter Endpoints", function(){
+       this.timeout(5000);
+       it('/getLatestImage should return image', function(done){
+            request(url)
+              .get('/getLatestImage')
+              .end(function (err, res){
+                  if(err){
+                     throw err;
+                  }
+                  res.should.have.status(200);
+                  res.should.have.header('content-type', "image/jpeg");
+                  done();
+             });
+       });
+       it('/getLatestGPS should return coordinates', function(done){
+           request(url)
+                .get('/getLatestGPS')
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    var response = JSON.parse(res.text);
+                    response.should.have.propert("lat");
+                    response.should.have.property("lon");
+                    done();
+                });
+       });
+
+
+   });
+});
