cd71183ed1d26181d8c6d0936f9a7227fc15bc97
Jake Reynolds <jakealexis@gmail.com>
2016-05-21 16:33:47 +0100

    instanbul from repo

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ npm install -g apidoc
 apidoc -i lib/ -o public/documentation/api -t apiDocsTemplate
 npm install -g jsdoc
 jsdoc lib/functions.js -d public/documentation/api
-npm install https://github.com/gotwarlost/istanbul
-istanbul cover node_modules/mocha/bin/_mocha -- -R spec
+#npm install https://github.com/gotwarlost/istanbul
+#istanbul cover node_modules/mocha/bin/_mocha -- -R spec
