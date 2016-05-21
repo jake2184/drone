fdf2c59b2d9fad6299d8d115658ce88f4f53e89d
Jake Reynolds <jakealexis@gmail.com>
2016-05-21 14:03:58 +0100

    Added npm install to install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 
-
+npm install -g apidoc
 apidoc -i lib/ -o public/documentation/api -t apiDocsTemplate
+npm install -g jsdoc
 jsdoc lib/functions.js -d public/documentation/api
+npm install -g instanbul
 istanbul cover node_modules/mocha/bin/_mocha -- -R spec
