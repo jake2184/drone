8adb327cdcd33545601a04e9b0d3917513ec28d7
Jake Reynolds <jakealexis@gmail.com>
2016-05-21 16:58:52 +0100

    Install dashboard

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 
+npm install dashboard
+
 npm install -g apidoc
 apidoc -i lib/ -o public/documentation/api -t apiDocsTemplate
 npm install -g jsdoc
-jsdoc lib/functions.js -d public/documentation/api
+jsdoc lib/functions.js -d public/documentation/functions
 #npm install https://github.com/gotwarlost/istanbul
 #istanbul cover node_modules/mocha/bin/_mocha -- -R spec
